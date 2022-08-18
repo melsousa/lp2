3ae40b62fa0e81ccfe23f8b72ffb098d47a82d4e
Melissa de Sousa <70774423+melsousa@users.noreply.github.com>
2022-08-17 21:39:17 -0300

    Update Whatsapp.py

--- interface/Whatsapp.py
+++ interface/Whatsapp.py
@@ -1,8 +1,10 @@
 from Enviar import Enviar
+from Usuario import Usuario
 
 class Whatsapp(Enviar):
     
     def execute(self, Usuario):
         print('Whastapp enviado com sucesso')
+        print('Usuário', Usuario.username)
         
     
