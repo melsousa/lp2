15c9be6049d4e794686db954f6acb66c17d296ba
Melissa de Sousa <70774423+melsousa@users.noreply.github.com>
2022-08-26 21:23:29 -0300

    Update AnswerChoice.py

--- aula 3/methodFactory/AnswerChoice.py
+++ aula 3/methodFactory/AnswerChoice.py
@@ -1,2 +1,18 @@
+from hashSet import HashSet
+from collections import defaultdict
+from Answer import Answer
+
 class AnswerChoice(Answer):
+    options = HashSet()
+
+    def __init__(self, options):
+        self.options = options
+
+    def type_answer(self):
+        return Answer.type_answer.CHOICE
+    
+    # get
+    def options(self):
+        return defaultdict.update
+
     
